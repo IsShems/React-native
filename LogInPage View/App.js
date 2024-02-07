bf4b6bab80ed3e87ec704e7edbe175ac11331e7f
Shems <96719853+IsShems@users.noreply.github.com>
2024-02-08 02:22:25 +0400

    Add files via upload

---
+++ LogInPage View/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, StyleSheet, ImageBackground } from "react-native";
+import MainPage from "./components/MainPage";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <MainPage />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+});
+
+export default App;
